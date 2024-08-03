a4aa37a23cc97a7ff795fc8f02f9e1e25c6182bc
WybsonSantana <wybson@hotmail.com>
2024-08-03 15:59:49 -0300

    fpped01 - aula5: Renomeio de colunas

--- fpped01-pipeline-de-dados/pipeline-dados/scripts/fusao_mercado_fev.py
+++ fpped01-pipeline-de-dados/pipeline-dados/scripts/fusao_mercado_fev.py
@@ -23,17 +23,49 @@ def ler_dados(path, tipo_arquivo):
         return ler_csv(path)
 
 
+def obter_nomes_colunas(dados):
+    return list(dados[0].keys())
+
+
+def renomear_colunas_dados_csv(dados_csv, key_mapping):
+    new_dados_csv = []
+    for old_dict in dados_csv:
+        dict_temp = {}
+        for old_key, value in old_dict.items():
+            dict_temp[key_mapping[old_key]] = value
+        new_dados_csv.append(dict_temp)
+    return new_dados_csv
+
+
 def main():
+    # Definição do caminho dos arquivos a serem lidos
     path_json = '..\\data\\raw\\dados_empresaA.json'
     path_csv = '..\\data\\raw\\dados_empresaB.csv'
 
+# leitura do arquivo de dados no formato json e obtenção dos nomes das colunas
     dados_json = ler_dados(path_json, 'json')
     print(dados_json[0])
-    print(type(dados_json))
+    nomes_colunas_json = obter_nomes_colunas(dados_json)
+    print(nomes_colunas_json)
 
+    # leitura do arquivo de dados no formato csv e obtenção dos nomes das colunas
     dados_csv = ler_dados(path_csv, 'csv')
     print(dados_csv[0])
-    print(type(dados_csv))
+    nomes_colunas_csv = obter_nomes_colunas(dados_csv)
+    print(nomes_colunas_csv)
+
+    # Definição do mapeamento dos nomes de colunas para os dados csv
+    key_mapping = {'Nome do Item': 'Nome do Produto',
+                   'Classificação do Produto': 'Categoria do Produto',
+                   'Valor em Reais (R$)': 'Preço do Produto (R$)',
+                   'Quantidade em Estoque': 'Quantidade em Estoque',
+                   'Nome da Loja': 'Filial',
+                   'Data da Venda': 'Data da Venda'}
+
+# Renomeando os nomes das colunas dos dados csv
+    dados_csv = renomear_colunas_dados_csv(dados_csv, key_mapping)
+    nomes_colunas_csv = obter_nomes_colunas(dados_csv)
+    print(nomes_colunas_csv)
 
 
 main()
